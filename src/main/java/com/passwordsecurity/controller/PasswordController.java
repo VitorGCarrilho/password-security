99a453261bdf9c36623cc7140ba356c1b7cf4a27
VitorGCarrilho <vitorg.carrilho@gmail.com>
2016-12-27 14:01:10 -0200

    Front-end index

---
+++ src/main/java/com/passwordsecurity/controller/PasswordController.java
@@ -0,0 +1,13 @@
+package com.passwordsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PasswordController {
+	
+	@RequestMapping("/home")
+	public String home(){ 
+		return "index";
+	}
+}
