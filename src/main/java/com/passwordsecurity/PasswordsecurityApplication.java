aeb53af7b7c85655bd9dcb5004702c44080669ef
VitorGCarrilho <vitorg.carrilho@gmail.com>
2016-12-26 22:21:00 -0200

    First Commit

---
+++ src/main/java/com/passwordsecurity/PasswordsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.passwordsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordsecurityApplication.class, args);
+	}
+}
---
+++ src/test/java/com/passwordsecurity/PasswordsecurityApplicationTests.java
@@ -0,0 +1,16 @@
+package com.passwordsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PasswordsecurityApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
